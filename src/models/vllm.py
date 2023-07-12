ab725732f6c5cfefa293250e9302af7f05358a39
MoeedDar <moeed.dar@gmail.com>
2023-07-12 17:22:09 +0100

    generator of vllm streams delta tokens

--- src/models/vllm.py
+++ src/models/vllm.py
@@ -13,5 +13,9 @@ class VLLM(Model):
     async def generate(self, prompt: str, args: GeneratorArgs = GeneratorArgs()) -> AsyncGenerator[str, None]:
         params = SamplingParams(temperature=args.temperature, top_p=args.top_p, top_k=args.top_k, max_tokens=args.max_tokens)
         generator = self.engine.generate(prompt, params, uuid.uuid4())
+        prev = ""
         async for output in generator:
-            yield output.outputs[0].text
+            curr = output.outputs[0].text
+            delta = curr[len(prev):]
+            prev = curr
+            yield delta
