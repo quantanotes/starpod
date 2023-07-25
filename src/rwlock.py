7e0f6b55767cc8078987aade128a92233e33bf04
MoeedDar <moeed.dar@gmail.com>
2023-07-25 01:23:35 +0100

    removed async lock

--- src/rwlock.py
+++ src/rwlock.py
@@ -1,4 +1,4 @@
-from contextlib import asynccontextmanager
+from contextlib import contextmanager
 import threading
 
 class RWLock:
@@ -26,7 +26,7 @@ class RWLock:
     def release_write(self):
         self._lock.release()
 
-    @asynccontextmanager
+    @contextmanager
     def read_lock(self):
         self.acquire_read()
         try:
@@ -34,7 +34,7 @@ class RWLock:
         finally:
             self.acquire_release()
 
-    @asynccontextmanager
+    @contextmanager
     def write_lock(self):
         self.acquire_write()
         try:
